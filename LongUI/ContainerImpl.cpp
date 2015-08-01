1793c1653c742eb2d33e5e6a5db6e3aee70e42c6
Dust Loong <dustpg@gmail.com>
2015-08-01 17:40:40 +0800

    continue to....

--- LongUI/ContainerImpl.cpp
+++ LongUI/ContainerImpl.cpp
@@ -226,60 +226,75 @@ void LongUI::UIContainer::Render(RenderType type) const noexcept {
 // 刷新边缘控件
 void LongUI::UIContainer::update_marginal_controls() noexcept {
     // 循环
-    // XXX: 优化
-    for (auto i = 0u; i < lengthof(this->marginal_control); ++i) {
-        auto ctrl = this->marginal_control[i];
-        if (!ctrl) continue;
-        float view[] = { 0.f, 0.f, 0.f, 0.f };
-        auto width_old = this->GetTakingUpWidth();
-        auto height_old = this->GetTakingUpHeight();
-        // TODO: 计算cross 大小
-        float tmp_float = 0.f;
-        switch (i)
-        {
-        case 0: // Left
-            if (this->marginal_control[UIMarginalable::Control_Top]) {
-                tmp_float = this->marginal_control[UIMarginalable::Control_Top]->marginal_width;
+    while (true) {
+        m_bControlSizeChanged = false;
+        // XXX: 优化
+        for (auto i = 0u; i < lengthof(this->marginal_control); ++i) {
+            auto ctrl = this->marginal_control[i];
+            if (!ctrl) continue;
+            float view[] = { 0.f, 0.f, 0.f, 0.f };
+            auto width_old = this->GetTakingUpWidth();
+            auto height_old = this->GetTakingUpHeight();
+            // TODO: 计算cross 大小
+            float tmp_float = 0.f;
+            switch (i)
+            {
+            case 0: // Left
+                if (this->marginal_control[UIMarginalable::Control_Top]) {
+                    tmp_float = this->marginal_control[UIMarginalable::Control_Top]->marginal_width;
+                }
+                // 初稿
+                ctrl->view_pos.x = m_orgMargin.left;
+                ctrl->view_pos.y = m_orgMargin.top + tmp_float;
+                ctrl->view_size.width = ctrl->marginal_width;
+                tmp_float = this->marginal_control[UIMarginalable::Control_Top] ?
+                    this->marginal_control[UIMarginalable::Control_Top]->marginal_width : 0.f;
+                ctrl->view_size.height = height_old - ctrl->view_pos.y - tmp_float;
+                break;
+            case 1: // Top
+                // 初稿
+                ctrl->view_pos.x = m_orgMargin.left;
+                ctrl->view_pos.y = m_orgMargin.top + view[0];
+                break;
+            case 2: // Right
+                // 初稿
+                ctrl->view_pos.x = width_old - ctrl->marginal_width;
+                ctrl->view_pos.y = m_orgMargin.top + tmp_float;
+                ctrl->view_size.width = ctrl->marginal_width;
+                tmp_float = this->marginal_control[UIMarginalable::Control_Top] ?
+                    this->marginal_control[UIMarginalable::Control_Top]->marginal_width : 0.f;
+                ctrl->view_size.height = height_old - ctrl->view_pos.y - tmp_float;
+                break;
+            case 3: // Bottom
+                    // 初稿
+                break;
             }
-            // 初稿
-            ctrl->view_pos.x = m_orgMargin.left;
-            ctrl->view_pos.y = m_orgMargin.top + tmp_float;
-            ctrl->view_size.width = ctrl->marginal_width;
-            tmp_float = this->marginal_control[UIMarginalable::Control_Top] ?
-                this->marginal_control[UIMarginalable::Control_Top]->marginal_width : 0.f;
-            ctrl->view_size.height = height_old - ctrl->view_pos.y - tmp_float;
-            break;
-        case 1: // Top
-            // 初稿
-            ctrl->view_pos.x = m_orgMargin.left;
-            ctrl->view_pos.y = m_orgMargin.top + view[0];
-            break;
-        case 2: // Right
-            // 初稿
-            ctrl->view_pos.x = width_old - ctrl->marginal_width;
-            ctrl->view_pos.y = m_orgMargin.top + tmp_float;
-            ctrl->view_size.width = ctrl->marginal_width;
-            tmp_float = this->marginal_control[UIMarginalable::Control_Top] ?
-                this->marginal_control[UIMarginalable::Control_Top]->marginal_width : 0.f;
-            ctrl->view_size.height = height_old - ctrl->view_pos.y - tmp_float;
-            break;
-        case 3: // Bottom
-            // 初稿
+            // 更新边界
+            ctrl->UpdateMarginalWidth();
+        }
+        // 修改完毕退出
+        if (!this->IsControlSizeChanged()) {
             break;
         }
-        // 更新边界
-        ctrl->UpdateMarginalWidth();
-        // 更新世界矩阵
-        ctrl->RefreshWorld();
-        // 坐标转换
-        D2D1_RECT_F clip_rect; ctrl->GetRectAll(clip_rect);
-        auto lt = LongUI::TransformPoint(ctrl->world, reinterpret_cast<D2D1_POINT_2F&>(clip_rect.left));
-        auto rb = LongUI::TransformPoint(ctrl->world, reinterpret_cast<D2D1_POINT_2F&>(clip_rect.right));
-        // 修改可视区域
-        ctrl->visible_rect.left = std::max(lt.x, this->visible_rect.left);
-        ctrl->visible_rect.top = std::max(lt.y, this->visible_rect.top);
-        ctrl->visible_rect.right = std::min(rb.x, this->visible_rect.right);
-        ctrl->visible_rect.bottom = std::min(rb.y, this->visible_rect.bottom);
+    }
+    this->SetControlSizeChanged();
+    // 更新
+    for (auto ctrl : this->marginal_control) {
+        // 刷新
+        if (ctrl) {
+            ctrl->Update();
+            // 更新世界矩阵
+            ctrl->RefreshWorld();
+            // 坐标转换
+            D2D1_RECT_F clip_rect; ctrl->GetRectAll(clip_rect);
+            auto lt = LongUI::TransformPoint(ctrl->world, reinterpret_cast<D2D1_POINT_2F&>(clip_rect.left));
+            auto rb = LongUI::TransformPoint(ctrl->world, reinterpret_cast<D2D1_POINT_2F&>(clip_rect.right));
+            // 修改可视区域
+            ctrl->visible_rect.left = std::max(lt.x, this->visible_rect.left);
+            ctrl->visible_rect.top = std::max(lt.y, this->visible_rect.top);
+            ctrl->visible_rect.right = std::min(rb.x, this->visible_rect.right);
+            ctrl->visible_rect.bottom = std::min(rb.y, this->visible_rect.bottom);
+        }
     }
 }
 
@@ -291,10 +306,6 @@ void LongUI::UIContainer::Update() noexcept  {
         // 刷新边缘控件
         if (this->flags & Flag_Container_ExistMarginalControl) {
             this->update_marginal_controls();
-            // 更新
-            for (auto ctrl : this->marginal_control) {
-                if(ctrl)  ctrl->Update();
-            }
         }
         this->AssertMarginalControl();
         // 本容器内容限制
