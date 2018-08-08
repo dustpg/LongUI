76fe0680c10fbc4e8deb4b14712c3bbb3d3b03f0
Dust Loong <dustpg@gmail.com>
2018-08-09 07:23:33 +0800

    add checkbox for demo5

---
+++ demo/demo5_fullcontrol/checkbox.cpp
@@ -0,0 +1,13 @@
+#include <control/ui_label.h>
+#include <control/ui_viewport.h>
+
+
+/// <summary>
+/// Initializes the viewport CheckBox.
+/// </summary>
+/// <param name="viewport">The viewport.</param>
+/// <returns></returns>
+void InitViewport_CheckBox(LongUI::UIViewport& viewport) noexcept {
+    using namespace LongUI;
+    auto& window = viewport.RefWindow();
+}
