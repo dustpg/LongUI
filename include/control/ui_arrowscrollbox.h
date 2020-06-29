7f25dc018a0630ab894bcdfd51dbfbdf6b29054a
Dust Loong <dustpg@gmail.com>
2020-06-30 00:49:37 +0800

    init arrowscrollbox

---
+++ include/control/ui_arrowscrollbox.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+/**
+* Copyright (c) 2014-2020 dustpg   mailto:dustpg@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "ui_box.h"
+#include "ui_image.h"
+
+// ui namespace
+namespace LongUI {
+    // arrow scroll box control
+    class UIArrowScrollBox : public UIControl {
+        // super class
+        using Super = UIControl;
+    protected:
+        // ctor
+        UIArrowScrollBox(const MetaControl&) noexcept;
+    public:
+        // class meta
+        static const  MetaControl   s_meta;
+        // dtor
+        ~UIArrowScrollBox() noexcept override;
+        // ctor
+        explicit UIArrowScrollBox(UIControl* parent = nullptr) noexcept : UIArrowScrollBox(UIArrowScrollBox::s_meta) { this->final_ctor(parent); }
+    public:
+        // do normal event
+        auto DoEvent(UIControl* sender, const EventArg& e) noexcept->EventAccept override;
+        // do mouse event
+        //auto DoMouseEvent(const MouseEventArg& e) noexcept->EventAccept override;
+        // update
+        void Update(UpdateReason reason) noexcept override;
+        // render this control only, [Global rendering and Incremental rendering]
+        void Render() const noexcept override;
+        // recreate/init device(gpu) resource
+        auto Recreate(bool release_only) noexcept->Result override;
+    protected:
+        // add child
+        void add_child(UIControl&) noexcept override;
+    private:
+    };
+    // get meta info for UIArrowScrollBox
+    LUI_DECLARE_METAINFO(UIArrowScrollBox);
+}
---
+++ src/control/ui_arrowscrollbox.cpp
@@ -0,0 +1,111 @@
+﻿#include <control/ui_arrowscrollbox.h>
+#include <core/ui_ctrlmeta.h>
+#include <debugger/ui_debug.h>
+
+
+// ui namespace
+namespace LongUI {
+    // UIArrowScrollBox类 元信息
+    LUI_CONTROL_META_INFO(UIArrowScrollBox, "arrowscrollbox");
+}
+
+
+/// <summary>
+/// Finalizes an instance of the <see cref="UIArrowScrollBox"/> class.
+/// </summary>
+/// <returns></returns>
+LongUI::UIArrowScrollBox::~UIArrowScrollBox() noexcept {
+    m_state.destructing = true;
+}
+
+
+/// <summary>
+/// Initializes a new instance of the <see cref="UIArrowScrollBox" /> class.
+/// </summary>
+/// <param name="meta">The meta.</param>
+LongUI::UIArrowScrollBox::UIArrowScrollBox(const MetaControl& meta) noexcept : Super(meta) {
+
+}
+
+/// <summary>
+/// Does the event.
+/// </summary>
+/// <param name="sender">The sender.</param>
+/// <param name="e">The e.</param>
+/// <returns></returns>
+auto LongUI::UIArrowScrollBox::DoEvent(UIControl* sender, 
+    const EventArg& e) noexcept -> EventAccept {
+    return Super::DoEvent(sender, e);
+}
+
+/// <summary>
+/// Does the mouse event.
+/// </summary>
+/// <param name="e">The e.</param>
+/// <returns></returns>
+//auto LongUI::UIArrowScrollBox::DoMouseEvent(const MouseEventArg & e) noexcept -> EventAccept {
+//    switch (e.type)
+//    {
+//    case LongUI::MouseEvent::Event_MouseWheelV:
+//        //LUIDebug(Hint) LUI_FRAMEID << e.type << endl;
+//        //return Event_Accept;
+//        break;
+//    case LongUI::MouseEvent::Event_MouseWheelH:
+//        LUIDebug(Hint) LUI_FRAMEID << e.type << endl;
+//        break;
+//    case LongUI::MouseEvent::Event_MouseEnter:
+//        break;
+//    case LongUI::MouseEvent::Event_MouseLeave:
+//        break;
+//    case LongUI::MouseEvent::Event_MouseMove:
+//        break;
+//    case LongUI::MouseEvent::Event_LButtonDown:
+//        break;
+//    case LongUI::MouseEvent::Event_LButtonUp:
+//        break;
+//    case LongUI::MouseEvent::Event_RButtonDown:
+//        break;
+//    case LongUI::MouseEvent::Event_RButtonUp:
+//        break;
+//    case LongUI::MouseEvent::Event_MButtonDown:
+//        break;
+//    case LongUI::MouseEvent::Event_MButtonUp:
+//        break;
+//    }
+//    return Super::DoMouseEvent(e);
+//}
+
+/// <summary>
+/// Renders this instance.
+/// </summary>
+/// <returns></returns>
+void LongUI::UIArrowScrollBox::Render() const noexcept {
+    return Super::Render();
+}
+
+/// <summary>
+/// update this instance.
+/// </summary>
+/// <returns></returns>
+void LongUI::UIArrowScrollBox::Update(UpdateReason reason) noexcept {
+    return Super::Update(reason);
+}
+
+/// <summary>
+/// Recreates this instance.
+/// </summary>
+/// <param name="release_only">if set to <c>true</c> [release only].</param>
+/// <returns></returns>
+auto LongUI::UIArrowScrollBox::Recreate(bool release_only) noexcept -> Result {
+    return Super::Recreate(release_only);
+}
+
+/// <summary>
+/// add child for this
+/// </summary>
+/// <param name="child"></param>
+/// <returns></returns>
+void LongUI::UIArrowScrollBox::add_child(UIControl& child) noexcept {
+    return Super::add_child(child);
+}
+
