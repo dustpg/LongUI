b812673ba6004b095a82aa2483e4a5d07603a953
Dust Loong <dustpg@gmail.com>
2020-07-09 00:38:55 +0800

    add platform base

---
+++ include/core/ui_platform.h
@@ -0,0 +1,137 @@
+﻿#pragma once
+/**
+* Copyright (c) 2014-2020 dustpg   mailto:dustpg@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "../core/ui_string.h"
+#include "../graphics/ui_cursor.h"
+#include "../core/ui_popup_window.h"
+#include "../core/ui_control_control.h"
+
+#if 1
+namespace LongUI {
+    // viewport
+    class UIViewport;
+    // window
+    class CUIWindow;
+    // mouse
+    struct MouseEventArg;
+    // dirty rect
+    struct DirtyRect {
+        // control
+        UIControl*          control;
+        // rectangle
+        RectF               rectangle;
+    };
+    // longui base platform
+    class CUIPlatform  {
+    protected:
+        // get window
+        auto window() noexcept->CUIWindow*;
+        // close window
+        void close_window() noexcept;
+        // get viewport
+        auto viewport() noexcept->UIViewport*;
+        // resize viewport
+        void resize_viewport(Size2F) noexcept;
+        // toggle ACCESS key
+        void toggle_access_key_display() noexcept;
+    public:
+        // ctor
+        CUIPlatform() noexcept;
+        // dtor
+        ~CUIPlatform() noexcept;
+    public:
+        // [thread safe] mouse event
+        void DoMouseEventTs(const MouseEventArg & args) noexcept;
+        // close popup window
+        void ClosePopup() noexcept;
+        // when key down/ up
+        void OnKey(InputEventArg arg) noexcept;
+        // [thread safe]when menu key
+        void OnMenuTs() noexcept;
+        // on access key
+        void OnAccessKey(uintptr_t) noexcept;
+        // when input a utf-16 char
+        void OnChar(char16_t ch) noexcept;
+        // when input a utf-32 char[thread safe]
+        void OnCharTs(char32_t ch) noexcept;
+        // on dpi changed
+        void OnDpiChanged(Size2F zoom, const RectL& rect) noexcept;
+    protected:
+        // title name
+        CUIString       titlename;
+        // rect of window
+        RectWHL         rect = {};
+        // adjust(border) rect of window 
+        RectL           adjust = { };
+        // rect of caret
+        RectF           caret = {};
+        // rect of foucs area
+        RectF           foucs = {};
+        // now cursor
+        CUICursor       cursor = { CUICursor::Cursor_Arrow };
+        // focused control
+        UIControl*      focused = nullptr;
+        // captured control
+        UIControl*      captured = nullptr;
+        // caret display control
+        UIControl*      careted = nullptr;
+        // now default control
+        UIControl*      now_default = nullptr;
+        // window default control
+        UIControl*      wnd_default = nullptr;
+        // focus list
+        ControlNode     focus_list = { nullptr };
+        // named list
+        ControlNode     named_list = { nullptr };
+        // popup window
+        CUIWindow*      popup = nullptr;
+        // common tooltip viewport
+        UIViewport*     common_tooltip = nullptr;
+        // ime count
+        uint16_t        ime_count = 0;
+        // saved utf16 char
+        char16_t        saved_utf16 = 0;
+        // popup type
+        PopupType       popup_type = PopupType::Type_Exclusive;
+        // draw focus
+        bool            draw_focus = false;
+        // draw caret
+        //bool            draw_caret = false;
+        // access key display
+        bool            access_key_display = false;
+        // dirty count for recording
+        uint16_t        dirty_count_recording = 0;
+        // dirty count for presenting
+        uint16_t        dirty_count_presenting = 0;
+        // dirty rect for recording
+        DirtyRect       dirty_rect_recording[LongUI::DIRTY_RECT_COUNT];
+        // dirty rect for presenting [+ 2 for safty]
+        RectF           dirty_rect_presenting[LongUI::DIRTY_RECT_COUNT + 2];
+        // access key map
+        UIControl*      access_key_map['Z' - 'A' + 1];
+    };
+}
+#endif
---
+++ src/core/ui_platform.cpp
@@ -0,0 +1,234 @@
+﻿// LUI
+#include <control/ui_viewport.h>
+#include <input/ui_kminput.h>
+#include <core/ui_platform.h>
+#include <core/ui_manager.h>
+#include <core/ui_window.h>
+// C/C++
+#include <cassert>
+// error beep
+extern "C" void longui_error_beep() noexcept;
+
+/// <summary>
+/// 
+/// </summary>
+/// <returns></returns>
+LongUI::CUIPlatform::~CUIPlatform() noexcept {
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <returns></returns>
+LongUI::CUIPlatform::CUIPlatform() noexcept {
+}
+
+/// <summary>
+/// Mouses the event.
+/// </summary>
+/// <param name="args">The arguments.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::DoMouseEventTs(const MouseEventArg & args) noexcept {
+    CUIDataAutoLocker locker;
+    UIControl* ctrl = this->captured;
+    if (!ctrl) ctrl = this->viewport();
+    // TODO: 处理返回值
+    ctrl->DoMouseEvent(args);
+}
+
+
+/// <summary>
+/// Closes the popup.
+/// </summary>
+/// <returns></returns>
+void LongUI::CUIPlatform::ClosePopup() noexcept {
+    if (this->popup) {
+        // 递归关闭
+        this->popup->ClosePopup();
+        auto& sub = this->popup->RefViewport();
+        sub.HosterPopupEnd();
+        this->viewport()->SubViewportPopupEnd(sub, this->popup_type);
+        this->popup->CloseWindow();
+        this->popup = nullptr;
+    }
+}
+
+
+/// <summary>
+/// Toggles the access key display.
+/// </summary>
+/// <returns></returns>
+void LongUI::CUIPlatform::toggle_access_key_display() noexcept {
+    auto& key = this->access_key_display; key = !key;
+    const EventArg arg{ NoticeEvent::Event_ShowAccessKey, key };
+    for (auto ctrl : this->access_key_map) if (ctrl)
+        ctrl->DoEvent(nullptr, arg);
+}
+
+
+
+// c
+extern "C" {
+    // char16 -> char32
+    char32_t impl_char16x2_to_char32(char16_t lead, char16_t trail) noexcept;
+}
+
+/// <summary>
+/// Called when [character].
+/// </summary>
+/// <param name="ch">The ch.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnChar(char16_t ch) noexcept {
+    char32_t ch32;
+    // UTF16 第一字
+    if (Unicode::IsHighSurrogate(ch)) { this->saved_utf16 = ch; return; }
+    // UTF16 第二字
+    else if (Unicode::IsLowSurrogate(ch)) {
+        ch32 = impl_char16x2_to_char32(this->saved_utf16, ch);
+        this->saved_utf16 = 0;
+    }
+    // UTF16 仅一字
+    else ch32 = static_cast<char32_t>(ch);
+    // 处理输入
+    this->OnCharTs(ch32);
+}
+
+/// <summary>
+/// Called when [character].
+/// </summary>
+/// <param name="ch">The ch.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnCharTs(char32_t ch) noexcept {
+    // IME输入优化
+    if (this->ime_count)--this->ime_count;
+    // TODO: 自己检查有效性?
+    if ((ch >= 0x20 && ch != 0x7f) || ch == '\t') {
+        // 直接将输入引导到焦点控件
+        if (const auto focused_ctrl = this->focused) {
+            CUIDataAutoLocker locker;
+            focused_ctrl->DoInputEvent({
+                LongUI::InputEvent::Event_Char, this->ime_count, ch
+                });
+        }
+    }
+    else assert(this->ime_count == 0 && "IME cannot input control char");
+}
+
+
+/// <summary>
+/// Called when [hot key].
+/// </summary>
+/// <param name="i">The index.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnAccessKey(uintptr_t i) noexcept {
+    // 存在弹出窗口就指向弹出窗口
+    if (this->popup) {
+        // TOOLTIP不算
+        if (this->popup_type != PopupType::Type_Tooltip) {
+            const auto popprivate = this->popup->pimpl();
+            popprivate->OnAccessKey(i);
+            return;
+        }
+    }
+    // 正式处理
+    CUIDataAutoLocker locker;
+    const auto ctrl = this->access_key_map[i];
+    if (ctrl && ctrl->IsEnabled() && ctrl->IsVisibleEx()) {
+        ctrl->DoEvent(nullptr, { NoticeEvent::Event_DoAccessAction, 0 });
+    }
+    else ::longui_error_beep();
+}
+
+/// <summary>
+/// Called when [dpi changed].
+/// </summary>
+/// <param name="zoom">The zoom parameter.</param>
+/// <param name="rect">The rect.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnDpiChanged(Size2F zoom, const RectL& rect) noexcept {
+    // dpi改变了
+    if (UIManager.flag & ConfigureFlag::Flag_NoAutoScaleOnHighDpi) return;
+    //float xdpi = float(uint16_t(LOWORD(wParam)));
+    //float ydpi = float(uint16_t(HIWORD(wParam)));
+    //float x = xdpi / float(LongUI::BASIC_DPI);
+    //float y = ydpi / float(LongUI::BASIC_DPI);
+    const auto x = zoom.width;
+    const auto y = zoom.height;
+    CUIDataAutoLocker locker;
+    auto& vp = *this->viewport();
+    auto& window = vp.RefWindow();
+    // 固定大小应该需要缩放窗口
+    if (window.config & CUIWindow::Config_FixedSize /*|| true*/) {
+        vp.JustResetZoom(x, y);
+        window.SetAbsoluteRect(rect);
+    }
+    // 不定大小应该懒得弄了
+    else {
+        vp.JustResetZoom(x, y);
+        const auto fw = static_cast<float>(this->rect.width);
+        const auto fh = static_cast<float>(this->rect.height);
+        this->resize_viewport({ fw, fh });
+    }
+}
+
+/// <summary>
+/// called when [menu key]
+/// </summary>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnMenuTs() noexcept {
+    CUIDataAutoLocker locker;
+    // 有popup就关闭
+    if (this->popup) this->ClosePopup();
+    // 没有就开关ACCESSKEY显示
+    else this->toggle_access_key_display();
+}
+
+/// <summary>
+/// Called when [key down/up].
+/// </summary>
+/// <param name="vk">The vk.</param>
+/// <returns></returns>
+void LongUI::CUIPlatform::OnKey(InputEventArg arg) noexcept {
+    // 直接将输入引导到焦点控件
+    if (const auto focused_ctrl = this->focused) {
+        assert(focused_ctrl->IsEnabled());
+        // 检查输出
+        const auto rv = focused_ctrl->DoInputEvent(arg);
+        if (rv != Event_Ignore) return;
+    }
+    // 如果无视事件则进行特殊处理
+    switch (arg.character)
+    {
+    case CUIInputKM::KB_ESCAPE:
+        // 检查释放Esc关闭窗口
+        if (this->viewport()->RefWindow().config
+            & CUIWindow::Config_EscToCloseWindow)
+            return this->viewport()->RefWindow().CloseWindow();
+        break;
+    case CUIInputKM::KB_RETURN:
+        // 回车键: 直接将输入引导到默认控件
+        if (const auto defc = this->now_default) {
+            if (defc->IsEnabled()) defc->DoInputEvent(arg);
+            return;
+        }
+        break;
+#if 0
+    case CUIInputKM::KB_SPACE:
+        if (const auto nowfocus = this->focused) {
+            if (nowfocus->IsEnabled()) nowfocus->DoInputEvent({ ekey, 0, key });
+            return;
+        }
+        break;
+#endif
+    case CUIInputKM::KB_TAB:
+        // Tab键: 聚焦上/下键盘焦点
+        this->draw_focus = true;
+        if (arg.event == InputEvent::Event_KeyUp) {
+            if (CUIInputKM::GetKeyState(CUIInputKM::KB_SHIFT))
+                this->viewport()->RefWindow().FocusPrev();
+            else
+                this->viewport()->RefWindow().FocusNext();
+        }
+        return;
+    }
+}
